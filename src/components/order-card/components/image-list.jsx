7152425f2fc75f94cd194a2a439f1bba50ed569a
Nikolay Brovkin <bronik004@gmail.com>
2022-12-06 11:09:53 +0300

    feat: added image-list counter

--- src/components/order-card/components/image-list.jsx
+++ src/components/order-card/components/image-list.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './image-list.module.scss';
 import img from '../../../images/ingredient.png';
 import { nanoid } from '@reduxjs/toolkit';
+import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 
 const ImageList = ({ ingredientsWithInfo }) => {
-
   return (
     <ul className={styles.list}>
       {ingredientsWithInfo.slice(0, 6).map((obj, index) =>
@@ -14,6 +14,13 @@ const ImageList = ({ ingredientsWithInfo }) => {
             style={{ zIndex: 6 - index }}
             className={styles.list__item}
           >
+            {obj.count > 1 && (
+              <Counter
+                count={obj.count}
+                size={'small'}
+              />
+            )}
+
             <img
               className={styles.list__img}
               src={obj.image_mobile}
@@ -31,7 +38,9 @@ const ImageList = ({ ingredientsWithInfo }) => {
               alt={obj.name}
             />
             {ingredientsWithInfo.length > 6 && (
-              <span className={`text text_type_digits-default ${styles.list__overlay_text}`}>
+              <span
+                className={`text text_type_digits-default ${styles.list__overlay_text}`}
+              >
                 +{ingredientsWithInfo.length - 6}
               </span>
             )}
