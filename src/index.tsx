78b6d27fa016f66f9eaa49d49b7725066ae85294
Nikolay Brovkin <bronik004@gmail.com>
2022-12-27 11:59:13 +0300

    migration: index.jsx to ts

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import store from './services/store';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root')
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
