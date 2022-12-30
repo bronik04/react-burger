a5cf9e7f2c1af047e9161fc46f186e53f7151eef
Nikolay Brovkin <bronik004@gmail.com>
2022-12-30 16:48:07 +0300

    migration: home-page.jsx.jsx to tsx

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {FC} from 'react';
 import styles from './profile.module.scss';
 import ProfileNav from '../../components/profile-nav/profile-nav';
 import UserForm from '../../components/user-form/user-form';
 
-const ProfilePage = () => {
+const ProfilePage: FC = () => {
   return (
     <div className={styles.container}>
       <ProfileNav />
