5b846b91eeee37d7ef7bb5f8ce9f8242cded8e73
Nikolay Brovkin <bronik004@gmail.com>
2023-01-03 15:19:56 +0300

    migration: protected-route.jsx to tsx

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,39 @@
+import { Redirect, Route, RouteProps, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import React, { FC } from 'react';
+import { selectAuth } from '../../services/auth/auth-selectors';
+
+type TProtectedRoute = RouteProps & {
+  onlyForAuth: boolean;
+};
+
+export const ProtectedRoute: FC<TProtectedRoute> = ({
+  onlyForAuth,
+  children,
+  ...rest
+}) => {
+  const { isAuth } = useSelector(selectAuth);
+  const location = useLocation();
+
+  if (!onlyForAuth && isAuth) {
+    // todo избавиться от any?
+    const { from }: any = location?.state || { from: { pathname: '/' } };
+    return (
+      <Route {...rest}>
+        <Redirect to={from} />
+      </Route>
+    );
+  }
+
+  if (onlyForAuth && !isAuth) {
+    return (
+      <Route {...rest}>
+        <Redirect to={{ pathname: '/login', state: { from: location } }} />
+      </Route>
+    );
+  }
+
+  return <Route {...rest}>{children}</Route>;
+};
+
+export default ProtectedRoute;
