e3ab9bf2b07492d4cf13e5fe6729c5c8f3fd9a90
Nikolay Brovkin <bronik004@gmail.com>
2023-01-03 11:24:35 +0300

    migration: forgot-password.jsx to tsx

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
+import React, {FC, useState} from 'react';
 import {
   Button,
   EmailInput,
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from '../../components/user-form/basic-form-styles.module.scss';
 import { Link, Redirect, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {fetchForgotPassword} from "../../services/auth/auth-async-thunks";
+import { useSelector } from 'react-redux';
+import { fetchForgotPassword } from '../../services/auth/auth-async-thunks';
+import { selectAuth } from '../../services/auth/auth-selectors';
+import { useAppDispatch } from '../../services/store';
 
-const ForgotPasswordPage = () => {
-  const { isAuth } = useSelector(state => state.auth);
+const ForgotPasswordPage: FC = () => {
+  const { isAuth } = useSelector(selectAuth);
   const history = useHistory();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const [email, setEmail] = useState('');
 
-  const onChangeEmail = e => {
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
-  const resetPassword = e => {
+  const resetPassword = (e:  React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch(fetchForgotPassword({ email }));
     history.replace({
@@ -33,21 +35,12 @@ const ForgotPasswordPage = () => {
 
   return (
     <div className={styles.container}>
-      <form
-        className={styles.form}
-        onSubmit={resetPassword}
-      >
+      <form className={styles.form} onSubmit={resetPassword}>
         <fieldset className={styles.wrapper}>
-          <h1
-            className={`text text_type_main-medium ${styles.heading}`}
-          >
+          <h1 className={`text text_type_main-medium ${styles.heading}`}>
             Восстановление пароля
           </h1>
-          <EmailInput
-            value={email}
-            name={email}
-            onChange={onChangeEmail}
-          />
+          <EmailInput value={email} name={email} onChange={onChangeEmail} />
           {email && (
             <Button
               extraClass={styles.form__button}
@@ -58,15 +51,10 @@ const ForgotPasswordPage = () => {
             </Button>
           )}
           <div className={styles.text__container}>
-            <p
-              className={`text text_type_main-default text_color_inactive`}
-            >
+            <p className={`text text_type_main-default text_color_inactive`}>
               Вспомнили пароль?
             </p>
-            <Link
-              className={styles.link}
-              to={'/login'}
-            >
+            <Link className={styles.link} to={'/login'}>
               Войти
             </Link>
           </div>
