dd0592a1e3dc08b837d2ef5011b21d1b0d2cd57d
Nikolay Brovkin <bronik004@gmail.com>
2023-01-03 15:27:57 +0300

    migration: order-list.jsx to tsx

--- src/components/order-list/order-list.tsx
+++ src/components/order-list/order-list.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {FC} from 'react';
 import OrderCard from '../order-card/order-card';
 import styles from './order-list.module.scss';
 import {useSelector} from "react-redux";
 import {selectOrders} from "../../services/web-socket/ws-selectors";
 
-const OrderList = () => {
-  const feeds = useSelector(selectOrders);
+const OrderList: FC = () => {
+  const orders = useSelector(selectOrders);
   return (
     <ul className={styles.container}>
       {
-        feeds.map(feed => <OrderCard key={feed._id} {...feed} />)
+        orders.map(order => <OrderCard key={order._id} {...order} />)
       }
     </ul>
   );
